675afff2dbca7f8bc94c8068e653aca38361bd93
islandbitcoin <dread@start9.com>
2022-11-25 20:42:12 -0500

    updated for x86 architecture

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -4,7 +4,8 @@ export const migration: T.ExpectedExports.migration = compat.migrations
   .fromMapping( {
       // 24.0.4 No migration needed 
       // 24.0.4.1 No migration needed 
-      // 24.0.4.2 No migration needed 
+      // 24.0.4.2 No migration needed
+      // 24.0.4.3 No migration needed  
     },
-    "24.0.4.3" 
+    "24.0.4.5" 
 );
