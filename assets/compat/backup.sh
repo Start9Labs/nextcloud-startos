3000cb8a22f2a57125c02f9dc55b06b2a9942e36
islandbitcoin <dread@start9.com>
2023-01-05 13:34:45 -0500

    re-add volumes

--- assets/compat/backup.sh
+++ assets/compat/backup.sh
@@ -4,9 +4,17 @@ set -e
 
 mkdir -p /mnt/backup/main
 mkdir -p /mnt/backup/nextcloud
+mkdir -p /mnt/backup/apps
+mkdir -p /mnt/backup/config
+mkdir -p /mnt/backup/data
+mkdir -p /mnt/backup/themes
 mkdir -p /mnt/backup/db
 mkdir -p /mnt/backup/dbconfig
 compat backup create /mnt/backup/main /root
 compat backup create /mnt/backup/nextcloud /var/www/html
+compat backup create /mnt/backup/apps /var/www/html/custom_apps
+compat backup create /mnt/backup/config /var/www/html/config
+compat backup create /mnt/backup/data /var/www/html/data
+compat backup create /mnt/backup/themes /var/www/html/themes
 compat backup create /mnt/backup/db /var/lib/postgresql/13
 compat backup create /mnt/backup/dbconfig /etc/postgresql/13
--- assets/compat/restore.sh
+++ assets/compat/restore.sh
@@ -4,5 +4,9 @@ set -e
 
 compat backup restore /mnt/backup/main /root
 compat backup restore /mnt/backup/nextcloud /var/www/html
+compat backup restore /mnt/backup/apps /var/www/html/custom_apps
+compat backup restore /mnt/backup/config /var/www/html/config
+compat backup restore /mnt/backup/data /var/www/html/data
+compat backup restore /mnt/backup/themes /var/www/html/themes
 compat backup restore /mnt/backup/db /var/lib/postgresql/13
 compat backup restore /mnt/backup/dbconfig /etc/postgresql/13
