e884a62da64214b47eb54422ce38543d4114d9ef
kn0wmad <39687477+kn0wmad@users.noreply.github.com>
2023-01-18 07:17:43 -0700

    Update for latest backup procedures (#24)
    Co-authored-by: Dread <34528298+islandbitcoin@users.noreply.github.com>

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,3 +1,2 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.7/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.7/util.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.7/healthUtil.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8-alpha2/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8-alpha2/healthUtil.ts";
