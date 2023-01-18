ecdd10564d9e8a08cbd4aef14caf9c4b9dcde2ce
J M <2364004+Blu-J@users.noreply.github.com>
2023-01-18 08:59:50 -0700

    Update healthChecks.ts

--- scripts/services/healthChecks.ts
+++ scripts/services/healthChecks.ts
@@ -1,8 +1,5 @@
 import { types as T, checkWebUrl, catchError } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
-  // deno-lint-ignore require-await
-  async "alive"(effects, duration) {
-    return checkWebUrl("http://nextcloud.embassy")(effects, duration).catch(catchError(effects))
-  },
+  "alive": checkWebUrl("http://nextcloud.embassy")
 };
