6237de570f01f210b81f72223ae7e2b881bd3273
Dominion5254 <musashidisciple@proton.me>
2025-03-06 13:52:47 -0700

    Migrate mimetypes at migration
    Co-authored-by: kn0wmad <39687477+kn0wmad@users.noreply.github.com>

--- migration-completion.sh
+++ migration-completion.sh
@@ -5,6 +5,8 @@ cp /usr/src/nextcloud/config/*.php /var/www/html/config/
 
 php /var/www/html/occ db:add-missing-indices
 
+php /var/www/html/occ maintenance:repair --include-expensive
+
 mkdir -p /root/migrations
 touch /root/migrations/$NEXTCLOUD_VERSION.complete
 touch /root/migrations/$(echo "$NEXTCLOUD_VERSION" | sed 's/\..*//g').complete
