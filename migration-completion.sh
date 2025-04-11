ce41a6e700ccdac3e3d2a6cb0ee3844daeceaf94
Shadowy Super Coder <musashidisciple@proton.me>
2025-04-11 13:25:52 -0600

    re-enable non-default apps on update

--- migration-completion.sh
+++ migration-completion.sh
@@ -9,7 +9,7 @@ php /var/www/html/occ maintenance:repair --include-expensive
 
 # Disable apps that aren't enabled by default
 declare -A default_map
-default_apps=('activity' 'calendar' 'circles' 'cloud_federation_api' 'comments' 'contacts' 'contactsinteraction' 'dashboard' 'dav' 'federatedfilesharing' 'federation' 'files' 'files_pdfviewer' 'files_reminders' 'files_rightclick' 'files_sharing' 'files_trashbin' 'files_versions' 'firstrunwizard' 'logreader' 'lookup_server_connector' 'nextcloud_announcements' 'notifications' 'oauth2' 'password_policy' 'photos' 'privacy' 'provisioning_api' 'recommendations' 'related_resources' 'serverinfo' 'settings' 'sharebymail' 'support' 'survey_client' 'systemtags' 'text' 'theming' 'twofactor_backupcodes' 'updatenotification' 'user_status' 'viewer' 'weather_status' 'workflowengine')
+default_apps=('activity' 'calendar' 'circles' 'cloud_federation_api' 'comments' 'contacts' 'contactsinteraction' 'dashboard' 'dav' 'federatedfilesharing' 'federation' 'files' 'files_downloadlimit' 'files_pdfviewer' 'files_reminders' 'files_rightclick' 'files_sharing' 'files_trashbin' 'files_versions' 'firstrunwizard' 'logreader' 'lookup_server_connector' 'nextcloud_announcements' 'notifications' 'oauth2' 'password_policy' 'photos' 'privacy' 'provisioning_api' 'recommendations' 'related_resources' 'serverinfo' 'settings' 'sharebymail' 'support' 'survey_client' 'systemtags' 'text' 'theming' 'twofactor_backupcodes' 'updatenotification' 'user_status' 'viewer' 'weather_status' 'workflowengine')
 
 for app in "${default_apps[@]}"; do
   default_map["$app"]=1
@@ -18,11 +18,21 @@ done
 enabled_apps=($(php /var/www/html/occ app:list | \
 awk '/^Enabled:/ {f=1; next} /^Disabled:/ {f=0} f && /^[[:space:]]+-/ {sub(/:$/, "", $2); print $2}'))
 
+declare -a disabled_map
+
+echo 'Disabling non-default apps'
 for enabled_app in "${enabled_apps[@]}"; do
   if [[ -z "${default_map[$enabled_app]}" ]]; then
     echo "Disabling non-default app: $enabled_app"
+    disabled_map+=($enabled_app)
     php /var/www/html/occ app:disable $enabled_app
   fi
+done
+
+echo 'Re-enabling non-default apps'
+for disabled_app in "${disabled_map[@]}"; do
+  echo "Re-enabling $disabled_app"
+  php /var/www/html/occ app:enable $disabled_app
 done
 
 mkdir -p /root/migrations
