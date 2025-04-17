149eddcf6fa79a3011d09f7634ddb95da9c81146
Shadowy Super Coder <musashidisciple@proton.me>
2025-04-17 17:06:53 -0600

    bump version

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -2,7 +2,7 @@ import { EmVer } from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/emver-lite/mo
 import { compat, matches, util, types as T } from "../deps.ts";
 import { getConfig } from "./getConfig.ts";
 
-const current = "29.0.14";
+const current = "29.0.14.1";
 const currentMajor = EmVer.parse(current).values[0];
 const minMajor = currentMajor - 1;
 
@@ -143,6 +143,20 @@ export const migration: T.ExpectedExports.migration = async (
           },
           true,
           { version: "29.0.14", type: "up" },
+        ),
+        down: () => {
+          throw new Error(
+            "Downgrades are prohibited per Nextcloud development team recommendations",
+          );
+        },
+      },
+      "29.0.14.1": {
+        up: compat.migrations.updateConfig(
+          (config) => {
+            return config;
+          },
+          true,
+          { version: "29.0.14.1", type: "up" },
         ),
         down: () => {
           throw new Error(
