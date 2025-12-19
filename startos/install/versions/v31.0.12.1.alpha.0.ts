0bfee3028108b82c996c043e54d75ca8ff96caf1
Matt Hill <mattnine@protonmail.com>
2025-12-19 12:27:55 -0700

    forgot file

---
+++ startos/install/versions/v31.0.12.1.alpha.0.ts
@@ -0,0 +1,15 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+// import { storeJson } from '../../fileModels/store.json'
+
+export const v_31_0_12_1_alpha_0 = VersionInfo.of({
+  version: '31.0.12:1-alpha.0',
+  releaseNotes: 'Updated for StartOS v0.4.0',
+  migrations: {
+    // up: async ({ effects }) => {
+    //   // @TODO migrate from 0351, including postgres update
+    //   await storeJson.write(effects, {})
+    // },
+    up: IMPOSSIBLE,
+    down: IMPOSSIBLE,
+  },
+})
