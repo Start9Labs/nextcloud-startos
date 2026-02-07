47a6ce4e83749542140b571c60d9dda112895299
Shadowy Super Coder <musashidisciple@proton.me>
2026-02-07 08:57:56 -0700

    Remove chown/chmod from migration-completion.sh (requires root, already handled by nextcloud-run.sh before entrypoint starts)

--- migration-completion.sh
+++ migration-completion.sh
@@ -46,7 +46,3 @@ php /var/www/html/occ maintenance:repair --include-expensive
 mkdir -p /root/migrations
 touch /root/migrations/$NEXTCLOUD_VERSION.complete
 touch /root/migrations/$(echo "$NEXTCLOUD_VERSION" | sed 's/\..*//g').complete
-
-# Ensure main volume is accessible to www-data (required for StartOS 0.4.0)
-chown -R root:www-data /root
-chmod -R g+rwX /root
