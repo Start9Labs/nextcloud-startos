a22308b9c7f5d022867ec45e51524d64b9096bb1
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:29 -0600

    Bump version to 32.0.8:1

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,8 +1,9 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_31_0_13_0 } from './v31.0.13.0'
 import { v_32_0_8_0 } from './v32.0.8.0'
+import { v_32_0_8_1 } from './v32.0.8.1'
 
 export const versionGraph = VersionGraph.of({
-  current: v_32_0_8_0,
-  other: [v_31_0_13_0],
+  current: v_32_0_8_1,
+  other: [v_32_0_8_0, v_31_0_13_0],
 })
---
+++ startos/versions/v32.0.8.1.ts
@@ -0,0 +1,16 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_32_0_8_1 = VersionInfo.of({
+  version: '32.0.8:1',
+  releaseNotes: {
+    en_US: 'Internal updates (start-sdk 1.3.3)',
+    es_ES: 'Actualizaciones internas (start-sdk 1.3.3)',
+    de_DE: 'Interne Aktualisierungen (start-sdk 1.3.3)',
+    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 1.3.3)',
+    fr_FR: 'Mises à jour internes (start-sdk 1.3.3)',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
